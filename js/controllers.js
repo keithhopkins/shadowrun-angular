a0d96c4ad1b143d7e78b8a44ecf094926f23fbfb
keithhopkins <knhopkins412@gmail.com>
2015-09-16 14:49:05 -0600

    add attributes to create

---
+++ js/controllers.js
@@ -0,0 +1,15 @@
+app.controller('HomeController', function($scope){
+  $scope.message='Home Page';
+});
+
+app.controller('CreateController', function($scope){
+  $scope.message='Create Page';
+});
+
+app.controller('CombatController', function($scope){
+  $scope.message='Combat Page';
+});
+
+app.controller('CharactersController', function($scope){
+  $scope.message='Characters Page';
+});
